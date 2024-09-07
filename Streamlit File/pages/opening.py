52d9680e410126f829d178ca68678d9a0499d473
jvontama96 <168260561+jvontama96@users.noreply.github.com>
2024-09-08 00:07:11 +0700

    Add files via upload

---
+++ Streamlit File/pages/opening.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Home Page",
+)
+
+st.markdown( 
+ """"
+  ## Welcome to my project on streamlit 👋
+  ## Recommendation System based on Weighted Rating , Content Based Filtering and Clustering Model for AirBnB 
+  
+  In streamlit , for now I will only cover EDA and Weighted Rating Recommendation System from the project.
+
+Complete notebook: https://colab.research.google.com/drive/1kfEZPAtU3rVSLBhIaJpsAAakJuwtg5Fd#scrollTo=LmpaJ555ZU0E 
+
+Dataset source: https://www.kaggle.com/datasets/ashishjangra27/airbnb-dataset/data
+
+  """
+)
+
