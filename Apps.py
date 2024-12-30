f56471acf5149b1994c707e7d692d9f4c0e6c77b
jvontama96 <168260561+jvontama96@users.noreply.github.com>
2024-12-31 01:08:57 +0700

    Update Apps.py

--- Apps.py
+++ Apps.py
@@ -1163,9 +1163,9 @@ with tabs[5]:
 
      contact1, contact2 = st.columns([1,20])
      with contact1:
-        st.image('Gmail.png', width=30)
-        st.image('LinkedIn.png', width=30)
-        st.image('Github.png', width=30)
+        st.image('Gmail.png', width=50)
+        st.image('LinkedIn.png', width=50)
+        st.image('Github.png', width=50)
      with contact2:
         st.markdown(
             """
