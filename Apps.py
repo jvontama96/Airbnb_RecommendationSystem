6a6870c5c71b6c3e089614509995ea3e34651453
jvontama96 <168260561+jvontama96@users.noreply.github.com>
2024-12-31 01:05:15 +0700

    Update Apps.py

--- Apps.py
+++ Apps.py
@@ -1163,9 +1163,9 @@ with tabs[5]:
 
      contact1, contact2 = st.columns([1,20])
      with contact1:
-        st.image('Gmail.png', width=47)
-        st.image('LinkedIn.png', width=40)
-        st.image('Github.png', width=43)
+        st.image('Gmail.png', width=30)
+        st.image('LinkedIn.png', width=30)
+        st.image('Github.png', width=30)
      with contact2:
         st.markdown(
             """
@@ -1181,8 +1181,3 @@ with tabs[5]:
             """,
             unsafe_allow_html=True,
         )
-
-     with contact2:
-        st.subheader('jevoncareer@gmail.com')
-        st.subheader('https://www.linkedin.com/in/sianiparjevon/')
-        st.subheader('https://www.github.com/jvontama96')
