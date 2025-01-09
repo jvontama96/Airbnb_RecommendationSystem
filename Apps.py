a94eb6becef214df95f7ed9e883b1ad24825fc80
jvontama96 <168260561+jvontama96@users.noreply.github.com>
2025-01-09 10:49:16 +0700

    Update Apps.py

--- Apps.py
+++ Apps.py
@@ -81,7 +81,7 @@ with tabs[0]:
     st.image("banner.png")
 
     st.markdown('''
-    <div style="font-size:20x; background-color:#e9f5ff; padding:10px; border-radius:10px;">
+    <div style="font-size:20x; background-color:#e9f5ff; padding:10px; border-radius:20px;">
     <strong>Hotel Booking App</strong> based on Airbnb data to simulate a Recommendation System using a K-means clustering model and predict prices for new listings using a Random Forest model. Recommends optimal prices to hotel owners for their newly input listings.<br><br>
     <a href="https://github.com/jvontama96/Airbnb_RecommendationSystem/tree/main" style="color:#1a73e8;" target="_blank">View Full Repository</a>
     </div>
